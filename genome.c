025bcdd8adad85fcb8a4324081f006495ec1516b
abxxbo <devavcado@gmail.com>
2022-09-22 19:13:42 -0500

    read entire file into buffer

--- genome.c
+++ genome.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+char* read_file(char* file){
+  char* buffer = 0;
+  long length;
+  FILE* fp = fopen(file, "rb");
+
+  if(fp){
+    fseek(fp, 0, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    buffer = malloc(length);
+    if(buffer) fread(buffer, 1, length, fp);
+    fclose(fp);
+  }
+
+  return buffer;
+}
+
+int main(int argc, char** argv){
+  // initializing
+  int ip = 0;
+  int vals[1024]; // vals[2] means ip=2
+
+  if(argc >= 2){
+    char* file = argv[1];
+    char* buffer__ = read_file(file);
+
+    // do something..
+  } else return 1;
+
   return 0;
 }
